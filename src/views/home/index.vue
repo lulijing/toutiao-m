5923fbbc97a71b561839093e3aeec20a35d3d524
lujingjing <429891624@qq.com>
2020-05-20 23:58:57 +0800

    day3y3y3_样式调整

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="home-container">
-首页
+    <van-nav-bar class="page-nav-bar">
+      <van-button class="search-btn" icon="search" slot="title" type="info" size="small" round>搜索</van-button>
+    </van-nav-bar>
+
+    <!-- 频道列表 -->
+    <!-- 通过v-model绑定当前激活标签对应的索引值，默认情况下启用第一个标签 -->
+    <!-- 通过animated属性可以开启切换标签内容时的转场动画 -->
+    <!-- 通过swipeable属性可以开启滑动切换标签页 -->
+    <van-tabs class="channel-tabs" v-model="active" animated swipeable>
+      <van-tab title="标签 1">内容 1</van-tab>
+      <van-tab title="标签 2">内容 2</van-tab>
+      <van-tab title="标签 3">内容 3</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+      <div slot="nav-right" class="hamburger-btn">
+        <i class="iconfont icongengduo"></i>
+      </div>
+    </van-tabs>
+
   </div>
 </template>
 
@@ -8,11 +25,75 @@
 export default {
   name: 'HomeIndex',
   data() {
-    return {}
+    return {
+      active: 0
+    }
   },
   methods: {}
 }
 </script>
 
-<style scoped lang="less">
+<style lang="less">
+.home-container {
+  .van-nav-bar__title {
+    max-width: unset;
+  }
+  .search-btn {
+    width: 555px;
+    height: 64px;
+    background-color: #5babfb;
+    border: none;
+    font-size: 28px;
+    .van-icon-search {
+      font-size: 32px;
+    }
+  }
+
+  /deep/ .channel-tabs {
+    .van-tabs__wrap {
+      height: 82px;
+    }
+    .van-tab {
+      border-right: 1px solid #edeff3;
+      min-width: 200px;
+      font-size: 30px;
+      color: #777777;
+    }
+    .van-tab--active {
+      color: #333333;
+    }
+    .van-tabs__nav--line {
+      padding-bottom: 0;
+    }
+    .van-tabs__line {
+      width: 32px !important;
+      height: 6px;
+      background-color: #3296fa;
+      bottom: 8px;
+    }
+    .hamburger-btn {
+      position: fixed;
+      right: 0;
+      width: 66px;
+      height: 82px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background-color: #fff;
+      opacity: 0.902;
+      i.iconfont {
+        font-size: 33px;
+      }
+      &:before {
+        content: "";
+        position: absolute;
+        left: 0;
+        width: 1px;
+        height: 58px;
+        background-image: url(~@/assets/gradient-gray-line.png);
+        background-size: contain;
+      }
+    }
+  }
+}
 </style>
